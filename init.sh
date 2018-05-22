c7e988f12eb32c2de3eac652ac1ed1e9ab518039
aknysh <andriy.knysh@gmail.com>
2018-05-21 22:59:28 -0400

    Add `init.sh`

---
+++ init.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Init Grafana and preconfigure data source to be influxdb
+/run.sh
+
+sleep 15
+
+curl -s -H "Content-Type: application/json" -X POST \
+    --data '{"name": "myinfluxdb", "type": "influxdb", "access": "proxy", "url": "http://influxdb:8086", "database": "k6", "isDefault": true}' \
+    http://admin:admin@localhost:3000/api/datasources
+
+kill -SIGINT %%
--- scenarios/config.js
+++ scenarios/config.js
@@ -1,4 +1,4 @@
 export let config = {
-    baseUrl: "< YOUR WEBSITE URL HERE, e.g. mywebsite.com >",
+    baseUrl: "< YOUR WEBSITE URL HERE, e.g. https://mywebsite.com >",
     userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36"
 };

d442e7f26667ec4ef9cc865245a5ff4667468a88
aknysh <andriy.knysh@gmail.com>
2018-05-23 11:18:37 -0400

    Add `setup` and `teardown` functions

--- README.md
+++ README.md
@@ -81,8 +81,14 @@ import {check} from "k6";
 import {options} from "./options.js";
 import {config} from "./config.js";
 
-export default function () {
+export function setup() {
     console.log("Options: " + JSON.stringify(options) + "\n");
+}
+
+export function teardown(data) {
+}
+
+export default function (data) {
     let res = http.get(config.baseUrl);
     check(res, {"Status is 200": (r) => r.status === 200});
 }
--- scenarios/scenario_all.js
+++ scenarios/scenario_all.js
@@ -3,9 +3,14 @@ import http from 'k6/http';
 import {options} from "./options.js";
 import {commonHeaders, config, mergeHeaders} from "./config.js";
 
-export default function () {
+export function setup() {
     console.log("Options: " + JSON.stringify(options) + "\n");
+}
 
+export function teardown(data) {
+}
+
+export default function (data) {
     group("page_01 - home", function () {
         let req, res;
         req = [{
